3eb65e6c5dd1a79002a064c30804deb54ad4ab63
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2022-02-24 20:48:18 -0600

    Add mavoglurant

---
+++ R/mavoglurant.R
@@ -0,0 +1,25 @@
+#' Mavoglurant PK data
+#'
+#' This was used in a  full PBPK model. This one was published for mavoglurant
+#' (Wendling et al. 2016).
+#'
+#' @format A data frame with 2,678 rows by 14 columns
+#' \describe{
+#'   \item{ID}{Subject ID}
+#'   \item{CMT}{Compartment Number}
+#'   \item{EVID}{Event ID}
+#'   \item{MDV}{Missing DV}
+#'   \item{DV}{Dependent Variable, Mavoglurant}
+#'   \item{AMT}{Dose Amount Keyword}
+#'   \item{TIME}{Time (hrs)}
+#'   \item{DOSE}{Dose}
+#'   \item{OCC}{Occasion}
+#'   \item{RATE}{Rate}
+#'   \item{AGE}{Age}
+#'   \item{SEX}{Sex}
+#'   \item{WT}{Weight}
+#'   \item{HT}{Height}
+#' }
+##' @source Wendling et al. 2016m
+##' @family nlmixr2 datasets
+"mavoglurant"
