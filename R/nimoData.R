7a9cde44604b33fa5362e67b2be51805935464b3
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2022-02-24 21:04:21 -0600

    Add nimoData

---
+++ R/nimoData.R
@@ -0,0 +1,24 @@
+#' Nimotuzumab PK data
+#'
+#' \describe{
+#'   \item{ID}{Subject ID}
+#'   \item{TIME}{Time (hrs)}
+#'   \item{AMT}{Dose Amount Keyword}
+#'   \item{RATE}{Rate}
+#'   \item{DV}{Dependent Variable, Nimotuzumab}
+#'   \item{TAD}{Time After Dose}
+#'   \item{CMT}{Compartment Number}
+#'   \item{OCC}{Occasion}
+#'   \item{MDV}{Missing DV}
+#'   \item{EVID}{Event ID}
+#'   \item{WGT}{Weight}
+#'   \item{BSA}{Body Surface Area}
+#'   \item{AGE}{Age}
+#'   \item{HGT}{Height}
+#'   \item{DOS}{Dose}
+#' }
+#' @format A data frame with 441 rows by 15 columns
+#' @source Rodriguez-Vera et al. 2015
+##' @family nlmixr2 datasets
+"nimoData"
+
